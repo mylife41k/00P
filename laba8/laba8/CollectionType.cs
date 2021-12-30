cf998d407412a484dad2ac9143cf97252f430528
MYLIFE41k <85196915+mylife41k@users.noreply.github.com>
2021-12-30 16:10:08 +0300

    Add files via upload

---
+++ laba8/laba8/CollectionType.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lab8
+{
+    public class CollectionType<T> : ICollectionType<T> where T : class 
+    {
+        private List<T> list = new List<T> { };
+        static int count = 0;
+
+        public void Add(T myList)
+        {
+            list.Add(myList);
+            count++;
+        }
+
+        public void Insert(int index, T myList)
+        {
+            if (index > list.Count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException("Out of range");
+            }
+            list.Insert(index, myList);
+            count++;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index > list.Count || index < 0)
+                {
+                    throw new Exception("Out of range");
+                }
+
+                return list[index];
+            }
+            set
+            {
+                if (index > list.Count || index < 0)
+                {
+                    throw new Exception("Out of range");
+                }
+                list[index] = value;
+            }
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write("{0}\t", list[i]);
+            }
+            Console.WriteLine();
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine();
+
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+            count = 0;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index > list.Count || index < 0)
+            {
+                throw new Exception("Out of range");
+            }
+            list.RemoveAt(index);
+            count--;
+        }  
+    }
+}
---
+++ laba8/laba8/ICollectionType.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab8
+{
+    public interface ICollectionType<T> where T : class 
+    {
+        void Add(T myList);
+        void Insert(int index, T myList);
+        void RemoveAt(int index);
+        void Clear();
+        void Show();
+        void Print();
+    }
+}
